1a3784d13ff2b1c522125d97e7749c2cb090db1b
Kenneth Soares <kensoares@gmail.com>
2022-07-15 23:07:24 +0200

    added functions to make inserting node easiser

--- linkedLists/main.c
+++ linkedLists/main.c
@@ -1,3 +1,14 @@
+/*
+      ____        ___  __                 _  __
+     /  _/______ / _ \/ /  ___  ___  ___ | |/_/
+    _/ // __/ -_) ___/ _ \/ _ \/ _ \/ -_)>  <
+   /___/\__/\__/_/  /_//_/\___/_//_/\__/_/|_|
+
+   Author: Kenneth Soares
+   Website: ken-soares.github.io
+   File: linkedLists/main.c
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,20 +26,49 @@ void printlist(node_t *head){
 	printf("\n");
 }
 
+node_t *create_new_node(int value){
+	node_t *result = malloc(sizeof(node_t));
+	result->value = value;
+	result->next = NULL;
+	return result;
+}
+
+
+// **head = on demande l'adresse du pointeur sur struct du nom de head
+node_t *insert_at_head(node_t **head, node_t *node_to_insert){
+	node_to_insert->next = *head;
+	*head = node_to_insert;
+	return node_to_insert;
+}
+
+void insert_after_node(node_t *node_to_insert_after, node_t *new_node){
+	new_node->next = node_to_insert_after->next;
+	node_to_insert_after->next = new_node;
+}
+
+node_t *find_node(node_t *head, int value){
+	node_t *tmp = head;
+	while(tmp != NULL){
+		if(tmp->value == value) return tmp;
+		tmp = tmp->next;
+	}
+
+	return NULL;
+}
+
 int main(){
-	node_t n1, n2, n3;
-	node_t *head;
-	n1.value = 45;
-	n2.value = 8;
-	n3.value = 32;
+	node_t *head = NULL;
+	node_t *tmp;
 
-	// link them up
+	for(int i = 0; i<25;i++){
+		tmp = create_new_node(i);
+		insert_at_head(&head,tmp);
+	}
 
-	head = &n3;
-	n3.next = &n2;
-	n2.next = &n1;
-	n1.next = NULL;
+	tmp = find_node(head,13);
+	printf("found node with value %d\n", tmp->value);
 
+	insert_after_node(tmp,create_new_node(75));
 	printlist(head);
 	return 0;
 }
