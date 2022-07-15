2f9902bf1e4243d8cd01b9221a5b3248511efe7e
Kenneth Soares <kensoares@gmail.com>
2022-07-15 22:03:22 +0200

    added stuff

---
+++ fun/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+
+int putchr(char c);
+void putstr(char *str);
+void putalphabet();
+
+#endif
---
+++ linkedLists/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+	int value;
+	struct node *next;
+} node_t;
+
+void printlist(node_t *head){
+	node_t *temporary = head;
+	while(temporary != NULL){
+		printf("%d - ",temporary->value);
+		temporary = temporary->next;
+	}
+	printf("\n");
+}
+
+int main(){
+	node_t n1, n2, n3;
+	node_t *head;
+	n1.value = 45;
+	n2.value = 8;
+	n3.value = 32;
+
+	// link them up
+
+	head = &n3;
+	n3.next = &n2;
+	n2.next = &n1;
+	n1.next = NULL;
+
+	printlist(head);
+	return 0;
+}
