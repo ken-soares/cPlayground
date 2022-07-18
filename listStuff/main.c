62e24f2f5f4da4f1b14bb94d78fe12e5e26940fc
Kenneth Soares <kensoares@gmail.com>
2022-07-18 19:00:58 +0200

    list exercices

---
+++ listStuff/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+void findMax(int *list){
+	int max = list[0];
+	for(int i = 0; i < sizeof(list)/sizeof(int)+1;i++){
+		if(list[i] > max){
+			max = list[i];
+		}
+	}
+	printf("The maximum value is %d\n", max);
+}
+
+void findMin(int *list){
+	int min = list[0];
+	for(int i = 0; i < sizeof(list)/sizeof(int)+1;i++){
+		if(list[i] < min){
+			min = list[i];
+		}
+	}
+	printf("The minimal value is %d\n", min);
+}
+int *askInput(int *list){
+	char end;
+	int i = 0;
+	while(tolower(end) != 'n'){
+		list = realloc(list,(i+1)*sizeof(int));
+		printf("enter an int> ");
+		scanf("%d", &list[i]);
+		printf("enter another int?> ");
+		scanf(" %c", &end);
+		i++;
+	}
+
+	return list;
+}
+
+int findSum(int *list){
+	int sum = 0;
+	for(int i = 0; i < sizeof(list)/sizeof(int)+1;i++){
+		sum += list[i];
+	}
+	return sum;
+}
+int main(void){
+
+	int *list = malloc(sizeof(int));
+	list = askInput(list);
+	findMax(list);
+	findMin(list);
+	int sum = findSum(list);
+	printf("The sum of all the items in the list is %d", sum);
+	return 0;
+}
