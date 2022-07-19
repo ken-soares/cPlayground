bc5414b35ad68e20b9ea6cbccc93f593a6f0be30
Kenneth Soares <96950526+ken-soares@users.noreply.github.com>
2022-07-19 22:08:50 +0200

    Update upperFc.c

--- stringStuff/upperFc.c
+++ stringStuff/upperFc.c
@@ -1,3 +1,13 @@
+/*
+      ____        ___  __                 _  __
+     /  _/______ / _ \/ /  ___  ___  ___ | |/_/
+    _/ // __/ -_) ___/ _ \/ _ \/ _ \/ -_)>  <
+   /___/\__/\__/_/  /_//_/\___/_//_/\__/_/|_|
+   Author: Kenneth Soares
+   Website: ken-soares.github.io
+   File: stringStuff/upperFC.c
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
