246aebb223f72ca98b71225d25606c5b54a5d7fe
Kenneth Soares <kensoares@gmail.com>
2022-07-21 17:28:15 +0200

    drawing shapes

--- SDL/main.c
+++ SDL/main.c
@@ -13,16 +13,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void SDL_ExitWithError(const char *message);
+
 int main(int argc, char **argv){
 
 	SDL_Window *window = NULL;
+	SDL_Renderer *renderer = NULL;
 
 	SDL_version nb;
 	SDL_VERSION(&nb);
 	printf("SDL ver %d.%d.%d", nb.major,nb.minor,nb.patch);
 	if(SDL_Init(SDL_INIT_VIDEO) != 0){
-		SDL_Log("Error in SDL init: %s\n",SDL_GetError());
-		exit(EXIT_FAILURE);
+		SDL_ExitWithError("Couldn't initialize video");
 	}
 
 	window = SDL_CreateWindow("First window in SDL",SDL_WINDOWPOS_CENTERED,
@@ -30,11 +32,53 @@ int main(int argc, char **argv){
 													500,500,0);
 
 	if(window ==NULL){
-		SDL_Log("Failed window creation: %s\n",SDL_GetError());
+		SDL_ExitWithError("Couldn't create window");
 	}
 
-	SDL_Delay(10000);
 
+	renderer = SDL_CreateRenderer(window,-1, SDL_RENDERER_SOFTWARE);
+
+	if(renderer == NULL){
+		SDL_ExitWithError("Couldn't create renderer");
+	}
+
+
+	if(SDL_SetRenderDrawColor(renderer,255,0,0, SDL_ALPHA_OPAQUE) != 0)
+		SDL_ExitWithError("couldn't change render color");
+
+	if(SDL_RenderDrawPoint(renderer,250,250)!= 0)
+		SDL_ExitWithError("impossible de dessiner un point");
+
+	if(SDL_SetRenderDrawColor(renderer,0,255,0, SDL_ALPHA_OPAQUE) != 0)
+		SDL_ExitWithError("couldn't change render color");
+
+	if(SDL_RenderDrawLine(renderer,0,0,200,200)!= 0)
+		SDL_ExitWithError("impossible de dessiner une ligne");
+
+
+	SDL_Rect rectangle;
+	rectangle.x = 300;
+	rectangle.y = 300;
+	rectangle.w = 100;
+	rectangle.h = 100;
+
+	if(SDL_SetRenderDrawColor(renderer,0,0,255, SDL_ALPHA_OPAQUE) != 0)
+		SDL_ExitWithError("couldn't change render color");
+
+	if(SDL_RenderFillRect(renderer,&rectangle)!= 0)
+		SDL_ExitWithError("impossible de dessiner un rectangle");
+
+	SDL_RenderPresent(renderer);
+	SDL_Delay(5000);
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return EXIT_SUCCESS;
+}
+
+void SDL_ExitWithError(const char *message){
+	SDL_Log("Error:%s > %s\n", message, SDL_GetError());
+	SDL_Quit();
+	exit(EXIT_FAILURE);
 }
